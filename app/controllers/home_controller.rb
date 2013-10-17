94cd3d8b78bd4bdc702c407c5d1ae3ce43e26639
Michael Brendan Dougherty <michaelbd@me.com>
2013-10-17 14:39:00 -0400

    Stripe gem, stripe views, unconnected to Pledges, do a db:migrate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-  if user_signed_in?
-    redirect_to :controller=&amp;gt;'dashboard', :action =&amp;gt; 'index'
+  # if user_signed_in?
+  #   redirect_to :controller=&amp;gt;'dashboard', :action =&amp;gt; 'index'
   end
 end
 
