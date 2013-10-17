f6de4dc25347ba7449e80e4a99cd6853fe1d319d
ScottDalessandro <scott@howdy-tech.com>
2013-10-17 11:13:58 -0400

    devise signup process

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def index
+  if user_signed_in?
+    redirect_to :controller=&amp;gt;'dashboard', :action =&amp;gt; 'index'
+  end
+end
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-	validates :first_name, presence: true
-	validates :last_name, presence: true
-	validates :email, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
 	has_many :campaigns
 	has_many :orders
